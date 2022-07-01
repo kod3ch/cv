2f973ec17e8d342d7cf8aaf0cf5bd835cd719c7a
kod3cz <artemenko.wlad@gmail.com>
2022-07-02 00:40:21 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php $pageTitle = "CV"; ?>
+<?php include('layouts/head.php');?>
+<body>
+    <div class="container">
+        <article class="message is-info info-box">
+            <div class="message-header">
+                <p>Witam</p>
+            </div>
+            <div class="message-body">
+                Strona jest w trakcie budowy, zajrzyj tu później :)
+            </div>
+        </article>
+    </div>
+</body>
+<?php include('layouts/footer.php');?>
+</html>
---
+++ pages/404.php
@@ -0,0 +1,20 @@
+<?php $pageTitle = "404"; ?>
+<?php include('../layouts/head.php');?>
+
+<body>
+    <div class="container">
+        <article class="message is-danger info-box">
+            <div class="message-header">
+                <p>404 | Nie znaleziono strony</p>
+            </div>
+            <div class="message-body">
+                Strona której szukasz nie jest dostępna na serwerze.
+            </div>
+
+        </article>
+        <a href="../index.php"><button class="button is-info">Strona domowa</button></a>
+    </div>
+</body>
+<?php include('../layouts/footer.php');?>
+
+</html>
